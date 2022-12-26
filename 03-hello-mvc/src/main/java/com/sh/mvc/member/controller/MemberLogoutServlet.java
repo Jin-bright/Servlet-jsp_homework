bc3529052fd487d286828b6d934be03498ed62da
J <77500290+Jin-bright@users.noreply.github.com>
2022-12-26 22:34:28 +0900

    Update MemberLogoutServlet.java

--- 03-hello-mvc/src/main/java/com/sh/mvc/member/controller/MemberLogoutServlet.java
+++ 03-hello-mvc/src/main/java/com/sh/mvc/member/controller/MemberLogoutServlet.java
@@ -19,19 +19,17 @@ public class MemberLogoutServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 0. 사용자입력값  - 없음  ex)mode =? 이런거 
+
 		
 		// 1. 로그아웃 처리
 		HttpSession session = request.getSession(false); //create 여부. 세션이 존재하지않는경우 생성하지 말고  null반환 
-	// HttpSession session = request.getSession(true); //기본값이 true임 어제한거 / 세션이 존재하지않는경우 생성하고 true반환
-		System.out.println( "session false처리 했을때 getid값(원래세션값인데,,): " + session.getId() ); //session객체를찾았다 ( coockie에서 value값 ) 
+		System.out.println( "session false처리 했을때 getid값(원래세션값인데,,): " + session.getId() );
 		if( session != null ) {
 			session.invalidate();
 		}
 		
 		// 2. redirect 
-		response.sendRedirect(request.getContextPath() + "/");  // 이렇게하면 아까 썻던 세션이 무효화 되어서 세션이 변경된걸알수있음
-		System.out.println( "redirect 후 세션 값 (똑같음/바뀌는건 application 탭내 세션값임 ): " + session.getId() );
+		response.sendRedirect(request.getContextPath() + "/"); 
 	}
 
 }
