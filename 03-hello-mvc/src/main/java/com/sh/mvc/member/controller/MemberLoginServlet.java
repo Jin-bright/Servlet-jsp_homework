ec8f6625f456b94cca88f5ba54de61d04b45b985
J <77500290+Jin-bright@users.noreply.github.com>
2022-12-26 22:33:39 +0900

    Update MemberLoginServlet.java

--- 03-hello-mvc/src/main/java/com/sh/mvc/member/controller/MemberLoginServlet.java
+++ 03-hello-mvc/src/main/java/com/sh/mvc/member/controller/MemberLoginServlet.java
@@ -28,10 +28,10 @@ public class MemberLoginServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//1. 사용자입력값 인코딩 처리 
-		request.setCharacterEncoding("utf-8"); //대소문자구분x
+		request.setCharacterEncoding("utf-8"); 
 		
 		//2. 사용자입력값을 자바변수에 담기 ( == 가져오기  )
-		String memberId = request.getParameter("memberId"); // id가 아닌 name값을 가져와야됨 
+		String memberId = request.getParameter("memberId"); 
 		String password = request.getParameter("password");
 		String saveId = request.getParameter("saveId");
 		
@@ -52,29 +52,20 @@ public class MemberLoginServlet extends HttpServlet {
 		
 		
 		if( member!= null && password.equals(member.getPassword())) {
-			//로그인 성공  System.out.println("로그인 성공 !");
-			// 로그인풀리는걸어떻게 방지할건가  => HttpSession객체 - 접속한 클라이언트(브라우저)별로 제공되는 객체 
-			// 얘의 생명주기는 브라우저가 첨만들어져서끌때까지 살아잇는거임 
-			
 			session.setAttribute("loginMember", member);
-			Cookie cookie = new Cookie("saveId", memberId); // 쿠키만듬
-			cookie.setPath(request.getContextPath()); // /mvc   /의 의미 =>  cookie를 전송할 url 설정인데 지정한 url하위경로에 모두 전송하는거임  
+			Cookie cookie = new Cookie("saveId", memberId);
+			cookie.setPath(request.getContextPath()); 
 		
 			
 			if(saveId != null) {
-				// 아이디 저장을 체크했을 경우임 (value값은 on임 )
 			//	Cookie cookie = new Cookie("saveId", memberId); // 쿠키만듬
-			//	cookie.setPath(request.getContextPath()); // /mvc   /의 의미 =>  cookie를 전송할 url 설정인데 지정한 url하위경로에 모두 전송하는거임  
-				cookie.setMaxAge(60*60*24*7); // 7일간 클라이언트에 저장한다는 뜻 
-				//만약 유효시간을 설정하지않으면 자동으로 session-cookie로 작동 / 브라우저 끄면 날라감 
-				//유효시간 설정 : setMaxage, setExpire 
-				//유효시간을 설정하면 persistent-cookie로 작동 / 지정한 기간동안 저장 
-				
+			//	cookie.setPath(request.getContextPath()); 
+				cookie.setMaxAge(60*60*24*7); // 7일간 클라이언트에 저장
 				
 				response.addCookie(cookie); // 응답헤더에 set-cookie로 전송이됨 
 				System.out.println("cookie뭐지 :  " +  cookie );
 			}
-			else { //아이디 저장 체크해제한 경우 -- 즉시 삭제 (키 value, path까지 ㅣㅇㄹ치해야됨 )  
+			else {
 		//		Cookie cookie = new Cookie("saveId", memberId); // 쿠키만듬
 		//		cookie.setPath(request.getContextPath());
 				cookie.setMaxAge(0);
@@ -91,7 +82,7 @@ public class MemberLoginServlet extends HttpServlet {
 		}
 		
 		//4. view단처리 -- redirect (url변경 - 새로고침 안되도록) 
-		response.sendRedirect(request.getContextPath()  + "/"  ); //클라이언트가 다시 요청할주소 
+		response.sendRedirect(request.getContextPath()  + "/"  ); 
 		
 	}
 
